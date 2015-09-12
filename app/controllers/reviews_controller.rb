110ef4cd57d40a20a462311c53dd2e6a88a86f99
cef2010 <cef2010@gmail.com>
2015-09-12 15:29:59 -0600

    rating form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,39 @@
 class ReviewsController < ApplicationController
   def new
+    @activity = Activity.find(params[:activity_id])
+    @review = Review.new
+  end
+
+  def create
+    binding.pry
+    @activity = Activity.find(params[:activity_id])
+    @review = Review.new(review_params)
+    @review.activity = @activity
+    @review.rating = params[:rating].to_i
+    @review.user = current_user
+    if @review.save
+      redirect_to activity_path(@review.activity_id)
+    else
+      render :new
+    end
   end
 
   def edit
+    @review = Review.find(params[:id])
   end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to activity_path(@review.activity_id)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:body, :rating)
+  end
+
 end
