dcd82adc3a030e76b810deaf4a4a118ea1552861
JordanSpencerWu <jordan.spencer.wu@gmail.com>
2015-09-12 16:12:02 -0600

    add CRUD reply

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,7 +1,29 @@
 class RepliesController < ApplicationController
   def new
+    @reply = Reply.new
+  end
+
+  def create
+    @reply = Reply.new(reply_params)
+
+    if @reply.save
+      redirect_to activity_path(@reply.review.activity)
+    else
+      render :new
+    end
   end
 
   def edit
+    @reply = Reply.find(params[:id])
+  end
+
+  def update
+    @reply = Reply.find(params[:id])
+
+    if @reply.update(reply_params)
+      redirect_to activity_path(@reply.review.activity)
+    else
+      render :edit
+    end
   end
 end
